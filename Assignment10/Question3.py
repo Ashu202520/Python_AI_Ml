1c01499653ff987af3d37061dca160c0a1143e5c
Ashu202520 <ashutoshdhavalshankh@gmail.com>
2026-01-19 19:36:13 +0530

    Assignments

--- Assignment10/Question3.py
+++
@@ -1,9 +0,0 @@
-def main():
-    No1 = int(input("Enter first number: "))
-    product = 1
-    for i in range(1, No1 + 1):
-        product = product * i
-    print("The product is:", product)
-
-if __name__ == "__main__":
-    main()
